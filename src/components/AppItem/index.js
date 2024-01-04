a9afabfbf8603459edbb833db94903a4809aadb0
chandramouli35 <tarigopulachandramouli1818@gmail.com>
2024-01-04 13:06:33 +0000

    solution

---
+++ src/components/AppItem/index.js
@@ -0,0 +1,15 @@
+// Write your code here
+import './index.css'
+
+const AppItem = props => {
+  const {appName, imageUrl} = props
+
+  return (
+    <li className="appItemContainer">
+      <img className="item-image" src={imageUrl} alt={appName} />
+      <p className="appItemName">{appName}</p>
+    </li>
+  )
+}
+
+export default AppItem
---
+++ src/components/TabItem/index.js
@@ -0,0 +1,26 @@
+// Write your code here
+// Write your code here
+
+import './index.css'
+
+const TabItem = props => {
+  const {tabId, displayText, isActive, clickTabItem} = props
+
+  const activeTabBtnClassName = isActive ? 'active-tab-btn' : ''
+
+  const onClickTabItem = () => {
+    clickTabItem(tabId)
+  }
+  return (
+    <li className="tabContainer">
+      <button
+        type="button"
+        className={`tab-btn ${activeTabBtnClassName}`}
+        onClick={onClickTabItem}
+      >
+        {displayText}
+      </button>
+    </li>
+  )
+}
+export default TabItem
